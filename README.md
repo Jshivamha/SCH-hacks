b61d04a06ae094e38ecd7bb09f5d50a653d33179
pnidhi26 <2016185@iiitdmj.ac.in>
2023-11-08 21:19:26 -0800

    commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## E-commerce Dashboard UI
+
+## Tech used:
+- React.js, Chart.js, TailwindCSS, SyncFusion 
+
+## In the project directory, you can run on local server:
+* npm start
+
+
+## Production Build:
+* npm run build
+
+# create app
+// npx create-react-app ./
+// npm install --legacy-peer-deps  
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2023-24 | Developed by <span><a href="https://prakashnv-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer"><u>Prakash Nidhi Verma</u></a> </span>
+    </p>
+  </div>
+);
+
+export default Footer;
